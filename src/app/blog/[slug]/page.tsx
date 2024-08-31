cc2abbed71acbedfc0a35a18de2f3d08914fa51c
Muhammad Vikri <muhammadvikrii99@gmail.com>
2024-08-31 11:35:03 +0700

    feat: add button share on blog

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import ButtonShare from '@/components/shared/ButtonShare';
+import ButtonShareLink from '@/components/shared/ButtonShareLink';
 import { getDetailContent } from '@/lib/notion';
 import { MoveLeft } from 'lucide-react';
 import type { Metadata } from 'next';
@@ -41,10 +43,17 @@ export default async function BlogDetail({
 
 	return (
 		<main className="space-y-7">
-			<Link href={'.'} className="flex flex-row items-center space-x-2">
-				<MoveLeft className="h-4 w-4" />
-				<p className="text-sm">back to all post</p>
-			</Link>
+			<div className="mb-12 flex flex-row items-center justify-between">
+				<Link href={'.'} className="flex flex-row items-center space-x-2">
+					<MoveLeft className="h-4 w-4" />
+					<p className="text-sm">back to all post</p>
+				</Link>
+
+				<div className="flex flex-row items-center space-x-2">
+					<ButtonShareLink />
+					<ButtonShare />
+				</div>
+			</div>
 			<div className="space-y-4">
 				<h1 className="text-xl font-medium">{blog.title}</h1>
 				<div
