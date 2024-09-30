e976763979c27746f4fc857cfed274cdc6dc2604
Muhammad Vikri <muhammadvikrii99@gmail.com>
2024-09-30 20:28:46 +0700

    feat: update about me

--- src/components/section/AboutMe.tsx
+++ src/components/section/AboutMe.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import Paragraph from '../shared/Paragraph';
 
 export default function AboutMe() {
-	return (
-		<section className="flex w-full flex-col">
-			<Paragraph title="About Me">
-				<p className="font-light">
-					I&apos;m an enthusiastic Frontend Developer with a strong focus
-					on crafting responsive and dynamic web applications. With
-					expertise in modern frameworks like React.js and Vue.js, I
-					specialize in building seamless user interfaces that not only
-					look great but also provide exceptional user experiences.
-				</p>
-			</Paragraph>
-		</section>
-	);
+  return (
+    <section className="flex w-full flex-col">
+      <Paragraph title="About Me">
+        <p className="font-light">
+          I&apos;m an enthusiastic Frontend and Mobile Developer with a strong
+          focus on crafting responsive and dynamic web applications and mobile
+          apps. With expertise in modern frameworks like React.js, Vue.js, React
+          Native, as well as Swift and Kotlin for mobile development, I
+          specialize in building seamless user interfaces that not only look
+          great but also provide exceptional user experiences.
+        </p>
+      </Paragraph>
+    </section>
+  );
 }
